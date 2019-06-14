277f99f13eef6ad577fca48d65fd340c75bd2f0b
NesrineMHB <51118715+NesrineMHB@users.noreply.github.com>
2019-06-14 16:24:37 +0200

    Update mongoProject.js

--- mongoProject.js
+++ mongoProject.js
@@ -7,27 +7,6 @@ server.use(express.static('.'))
 server.use(bodyParser.urlencoded({ extended : false }))
 
 
-/*const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});*/
 
 // Inclusion de Mongoose
 const mongoose = require('mongoose')
@@ -118,3 +97,28 @@ mongoose.connection.once('open',function(){
 }).on('error',function(error){
   console.log('error',error)
 });*/
+
+
+
+/*const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});*/
+
